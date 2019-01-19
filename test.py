11290eefcb5fa5d034e0763494d557094f24c85d
shihern <shihern2708@gmail.com>
2019-01-20 01:34:17 +0800

    Add simple code

--- test.py
+++ test.py
@@ -1,2 +1,39 @@
 import nltk
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords, wordnet
 
+lem = WordNetLemmatizer()
+
+filename = 'test.txt'
+
+
+def remove_noise(text):
+    return [(word, tag) for (word, tag) in text if word not in stopwords.words('english')]
+
+
+def get_wordnet_pos(treebank_tag):
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return ''
+
+
+def main():
+    with open(filename) as fp:
+        m = fp.read()
+        tokenized = nltk.word_tokenize(m)
+        tagged = nltk.pos_tag(tokenized)
+        denoised = remove_noise(tagged)
+        lemmatized = [lem.lemmatize(word, 'v') for (word, tag) in denoised]
+
+        print(' '.join(lemmatized))
+
+
+if __name__ == '__main__':
+    main()    
