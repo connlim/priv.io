d908b44a0d81c8610bd6f0ac05259468f94eb0bf
shihern <shihern2708@gmail.com>
2019-01-20 02:38:02 +0800

    Add basic extraction

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import nltk
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import stopwords, wordnet
 
+import json
+
 lem = WordNetLemmatizer()
 
 filename = 'test.txt'
@@ -25,12 +27,22 @@ def get_wordnet_pos(treebank_tag):
 
 
 def main():
+    with open('keywords.json') as fp:
+        json_data = json.loads(fp.read())
     with open(filename) as fp:
         m = fp.read()
         tokenized = nltk.word_tokenize(m)
         tagged = nltk.pos_tag(tokenized)
-        denoised = remove_noise(tagged)
-        lemmatized = [lem.lemmatize(word, 'v') for (word, tag) in denoised]
+        # denoised = remove_noise(tagged)
+        lemmatized = [lem.lemmatize(word, 'v') for (word, tag) in tagged]
+
+        results = {}
+        for key in json_data:
+            instances = [(x, i) for i, x in enumerate(lemmatized) for word in json_data[key] if x == word]
+            if len(instances) > 0:
+                results[key] = instances 
+
+        print(results)
 
         print(' '.join(lemmatized))
 
