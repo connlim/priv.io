8eab2a04be7b745dd64156ece77a6def48a84eff
shihern <shihern2708@gmail.com>
2019-01-20 08:21:07 +0800

    Extract sentences based on keywords

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import stopwords, wordnet
 
 import json
+import re
 
 lem = WordNetLemmatizer()
 
@@ -26,6 +27,18 @@ def get_wordnet_pos(treebank_tag):
         return ''
 
 
+def get_sentence(pos, text):
+    before = text[pos::-1]
+    try:
+        start = len(before) - before.index('.')
+    except ValueError:
+        start = 0
+    try:
+        end = text[pos:].index('.') + pos + 1
+    except ValueError:
+        end = len(text)
+    return text[start:end]
+
 def main():
     with open('keywords.json') as fp:
         json_data = json.loads(fp.read())
@@ -36,15 +49,25 @@ def main():
         # denoised = remove_noise(tagged)
         lemmatized = [lem.lemmatize(word, 'v') for (word, tag) in tagged]
 
+        print(' '.join(lemmatized), end='\n\n')
+
         results = {}
         for key in json_data:
             instances = [(x, i) for i, x in enumerate(lemmatized) for word in json_data[key] if x == word]
             if len(instances) > 0:
-                results[key] = instances 
+                results[key] = instances
+        print(results, end='\n\n')
 
-        print(results)
-
-        print(' '.join(lemmatized))
+        # Get sentence
+        sentences = {}
+        for key in results:
+            sentences[key] = {}
+            for word in results[key]:
+                s = ' '.join(get_sentence(word[1], tokenized))
+                s = re.sub(r' ([.,!?\)])', r'\1', s)
+                s = re.sub(r'(\() ', r'\1', s)
+                sentences[key][word] = s
+        print(sentences)
 
 
 if __name__ == '__main__':
